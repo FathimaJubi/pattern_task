6144b17947716f801eade3cfaa204978c4ce48e5
FathimaJubi <144306597+FathimaJubi@users.noreply.github.com>
2024-03-14 14:37:16 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,22 @@
+
+def pattern(row,column):
+        for i in range(1,row+2):
+                if i==1:
+                   for j in range(1,column-1):
+                        if j<=row:
+                           print(" ___  ",end="  ")
+                   print()
+                else:
+                   for j in range(1,column-1):
+                        if j<=row:
+                                print("/   ",end="")
+                                print('\\',end="")
+                                if j!=row:
+                                   print("___",end="")
+                   print()
+                   for j in range(1,column-1):
+                        if j<=row:
+                           print("\___/  ",end=" ")
+                   print()
+pattern(int(input("number of rows:")),
+        int(input("number of columns:")))
